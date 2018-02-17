a352ddf950d0d542d34e8baebd4e76f28da56c65
Stabz <dmit0038@edu.easj.dk>
2018-02-17 15:59:35 +0100

    first try
    trying to commit to master for the first time

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour {
+
+    public float health = 20;
+    public float strength = 5;
+    public float accuracy = 5;
+    public float defence = 5;
+    public float agility = 5;
+
+    private GameObject pawn;
+
+	void Start () {
+
+        pawn = GameObject.FindGameObjectWithTag("sup");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DealDmg(this.strength, 5f);
+        ReceiveDmg(5f);
+	}
+
+
+
+    private void DealDmg(float strength,float accuracy)
+    {
+        //the returning dmg
+        float dmg = 0;
+
+        //random number, use accuracy.
+        float hitChance = Random.Range(0,101);
+        hitChance = +accuracy;
+
+
+        if (hitChance>49)
+        {
+            dmg = strength * 1.1f;
+        }
+
+        
+        pawn.SendMessage("ReceiveDmg",dmg);
+
+        Debug.Log("hit");
+
+
+    }
+
+    private void ReceiveDmg(float incDmg)
+    {
+
+
+        float dmg = incDmg*(1-(this.defence/100));
+
+        health = health - dmg;
+
+        Debug.Log("health: " + health);
+    }
+
+}
---
+++ Assets/Scripts/SupStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupStats : MonoBehaviour {
+
+    private GameObject Sup;
+
+
+	void Start () {
+        Sup = GameObject.FindGameObjectWithTag("map");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
