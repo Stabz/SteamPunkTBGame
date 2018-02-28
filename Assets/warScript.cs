333e5a83e9cfff2b8dcd624543d54aa3b1df9bc5
Gitgest <aable24@gmail.com>
2018-02-28 13:17:13 +0100

    Updated game to allow damaging and destroying a gameobject.

---
+++ Assets/warScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class warScript : MonoBehaviour {
+
+    public int health = 10;
+
+    public GameObject target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void DealDmg()
+    {
+        target.SendMessage("RecieveDmg");
+    }
+
+    void RecieveDmg()
+    {
+        Debug.Log("Av for helvede");
+        health--;
+        if (health == 0) Destroy(this.gameObject);
+    }
+
+}
+
