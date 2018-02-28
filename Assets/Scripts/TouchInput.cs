8251ba052f715cad9bb503f8f3120f22217bb30a
Abdul2010 <abdu0445@edu.easj.dk>
2018-02-28 11:54:10 +0100

    TouchInput

---
+++ Assets/Scripts/TouchInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour {
+
+
+    bool Select;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (int i = 0; i < Input.touchCount; i++)
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                Debug.Log("Touch detected");
+
+                var test = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
+                RaycastHit2D hit = Physics2D.Raycast(test, (Input.GetTouch(i).position));
+                if (hit.collider && hit.collider.tag == "sup")
+                {
+                    hit.collider.SendMessage("ReceiveDmg");
+                    Debug.Log("Dmg?");
+                }
+            }
+
+        }
+    }
+    }
