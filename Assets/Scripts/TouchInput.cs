43dd959402c390e688192443c36b1f21fd9362ce
Gitgest <aable24@gmail.com>
2018-03-21 13:33:23 +0100

    Added range functionality, function describtions.
    Added a checkRange() function in TouchInput.cs and a doAttack() function
    for the sendMessage() function. Added describtions for both.
    Also added a HealthBarScript.cs which should contain healthbar
    functionality, hoping to make it a bunch of bones that disappear as the
    player characters lose health.

--- Assets/Scripts/TouchInput.cs
+++ Assets/Scripts/TouchInput.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +10,8 @@ public class TouchInput : MonoBehaviour
     bool Select;
     private List<GameObject> targetList = new List<GameObject>();
     public GameObject target;
-
+    private float distance;
+    private bool inRange = false;
     // Use this for initialization
     void Start()
     {
@@ -29,8 +31,7 @@ public class TouchInput : MonoBehaviour
                 var test = Camera.main.ScreenToWorldPoint(Input.GetTouch(i).position);
                 RaycastHit2D hit = Physics2D.Raycast(test, (Input.GetTouch(i).position));
 
-                // Add hitcollider's GameObject to List
-                
+                // Add hitcollider's GameObject to List       
                 if (hit.collider.gameObject != null)
                 {
                     targetList.Add(hit.collider.gameObject);
@@ -40,7 +41,7 @@ public class TouchInput : MonoBehaviour
                     }
                 }
 
-                if (targetList[0].tag == "enemy")
+                if (targetList[0].tag == "enemy" || targetList[1].tag == "Player")
                 {
                     targetList.Clear();
                 }
@@ -61,21 +62,55 @@ public class TouchInput : MonoBehaviour
     void attackTouch()
     {
 
-            if (targetList.Count == 2)
-                target = targetList[1];
+        if (targetList.Count == 2)
+            target = targetList[1];
+        {
+            CheckRange();
+            if (inRange == true)
             {
-
-
-                if (targetList[0].tag == "Player" && targetList[1].tag == "enemy")
-                {
-                    this.target = targetList[1];
-                    targetList[1].SendMessage("DealDmg", targetList[0].GetComponent<warScript>().attack);
-                    audioSource.Play();
-                    targetList.Clear();
-                }
-                targetList.Clear();
+                doAttack();
             }
+            targetList.Clear();
         }
     }
+
+    /// <summary>
+    /// Checks the range between the player unit attacking and the target
+    /// </summary>
+    /// <returns name="inRange"></returns>
+    bool CheckRange()
+    {
+        float dist = Vector2.Distance(targetList[0].transform.position, targetList[1].transform.position);
+        if(Math.Abs(dist) <= Math.Abs(targetList[0].GetComponent<warScript>().range))
+        {
+            inRange = true;
+            Debug.Log("Within Range");
+            Debug.Log("The distance between the player and the target is: " + dist);
+        }
+        else
+        {
+            inRange = false;
+            Debug.Log("Not in range");
+            Debug.Log("The distance between the player and the target is: " + dist);
+        }
+        return inRange;
+    }
+
+    /// <summary>
+    /// Function to set target and send message to the target.
+    /// </summary>
+    void doAttack()
+    {
+        if (targetList[0].tag == "Player" && targetList[1].tag == "enemy")
+        {
+            this.target = targetList[1];
+            targetList[1].SendMessage("DealDmg", targetList[0].GetComponent<warScript>().attack);
+            audioSource.Play();
+            targetList.Clear();
+        }
+    }
+}
+
+
 
 
